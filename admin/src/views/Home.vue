ebe890c4f5eee1908c9c3eaac624332ecd257075
yifan.wu <yifan.wu@ubtrobot.com>
2020-03-20 17:14:52 +0800

    初始化vue

---
+++ admin/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <loginIn msg="管理后台"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import loginIn from '@/components/loginIn.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    loginIn,
+  },
+};
+</script>
